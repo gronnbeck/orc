dcacbd25dc517ca1db3bb236e7a655436323faf2
Ken Oscar Grønnbeck <kengroenn@gmail.com>
2020-01-18 13:21:29 +0000

    Add basic endpoint using WAI

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
+import Network.HTTP.Types (status200)
 
 main :: IO ()
-main = someFunc
+main = run 3000 $ \_req send ->
+  send $ responseBuilder
+    status200
+    [("Content-Type", "text/plain; charset=utc-8")]
+    "Hello from WAI"
