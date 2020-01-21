7bc307344b245b0f6722d4bc8950aab13c20e197
Ken Oscar Grønnbeck <kengroenn@gmail.com>
2020-01-21 18:42:00 +0000

    Add basic record and ToJSON typeclass for the record

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
@@ -8,6 +9,15 @@ import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200, status404, status405)
 import Data.Text as T
 import Data.Text.Encoding
+import Data.Aeson (ToJSON, toEncoding, (.=), object, encode, genericToEncoding, defaultOptions)
+import GHC.Generics
+import Data.ByteString.Builder (lazyByteString)
+
+data Job = Job { id :: String, status :: String }
+  deriving (Generic, Show)
+
+instance ToJSON Job where
+  toEncoding = genericToEncoding defaultOptions
 
 badRequestMethod :: Response
 badRequestMethod = responseBuilder status405 [] "Bad request method"
@@ -16,14 +26,18 @@ main :: IO ()
 main = run 3000 $ \req send ->
   case pathInfo req of
     [] -> case requestMethod req of
-      "GET" -> send $ responseBuilder
-                status200
-                [("Content-Type", "text/plain; charset=utc-8")]
-                "Hello from WAI"
-       _    -> pure badRequestMethod
+            "GET" -> send $ responseBuilder
+                      status200
+                      [("Content-Type", "text/plain; charset=utc-8")]
+                      "Hello from WAI"
+            _     -> send badRequestMethod
     ["hello", name] -> send $ responseBuilder
             status200
             []
             (encodeUtf8Builder (T.concat [T.pack "Hello, ", name]))
+    ["job"] -> send $ responseBuilder
+                status200
+                []
+                (lazyByteString (encode (Job "uuid" "pending")))
     _ ->  send $ responseBuilder status404 []
             "These are not the droid you are looking for"
