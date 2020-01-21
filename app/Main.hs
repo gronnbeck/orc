4c155ad2455cfaa24bbd03308f047251bb30e662
Ken Oscar Grønnbeck <kengroenn@gmail.com>
2020-01-21 17:20:39 +0000

    Experiment with request methods filtering

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,22 @@ module Main where
 import Lib
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
-import Network.HTTP.Types (status200, status404)
+import Network.HTTP.Types (status200, status404, status405)
 import Data.Text as T
 import Data.Text.Encoding
+
+badRequestMethod :: Response
+badRequestMethod = responseBuilder status405 [] "Bad request method"
 
 main :: IO ()
 main = run 3000 $ \req send ->
   case pathInfo req of
-    [] -> send $ responseBuilder
-            status200
-            [("Content-Type", "text/plain; charset=utc-8")]
-            "Hello from WAI"
+    [] -> case requestMethod req of
+      "GET" -> send $ responseBuilder
+                status200
+                [("Content-Type", "text/plain; charset=utc-8")]
+                "Hello from WAI"
+       _    -> pure badRequestMethod
     ["hello", name] -> send $ responseBuilder
             status200
             []
